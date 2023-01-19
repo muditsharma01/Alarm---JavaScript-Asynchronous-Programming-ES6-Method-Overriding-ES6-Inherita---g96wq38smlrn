a0f08aa08f668273de6b5af6eb13f28f97a9296e
muditsharma01 <muditshar001@gmail.com>
2023-01-19 23:24:16 +0530

    Update alarmEdit.jsx

--- src/components/alarmEdit.jsx
+++ src/components/alarmEdit.jsx
@@ -13,10 +13,10 @@ const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function AlarmForm(props) {
+export default function AlarmEditForm(props) {
 
-    const [label, setLabel] = React.useState("");
-    const [alarmTime, setAlarmTime] = React.useState("07:30");
+    const [label, setLabel] = React.useState(props.label);
+    const [alarmTime, setAlarmTime] = React.useState(props.alarmTime);
     return (
         <>
             <Dialog
@@ -26,16 +26,16 @@ export default function AlarmForm(props) {
                 onClose={props.onClose}
                 aria-describedby="alert-dialog-slide-description"
             >
-                <DialogTitle>Add an alarm</DialogTitle>
+                <DialogTitle>Edit alarm</DialogTitle>
                 <DialogContent>
                     <DialogContentText id="alert-dialog-slide-description">
                         <Box display="flex" flexDirection='column'>
-                            <TextField id="label" label="Alarm Label" variant="outlined" value={label} onChange={(event) => setLabel(event.target.value)} />
+                            <TextField id={props.alarmid + '-label'} label="Alarm Label" variant="outlined" value={label} onChange={(event) => setLabel(event.target.value)} />
                             <TextField
-                                id="add-time"
                                 label="Alarm clock"
                                 type="time"
                                 value={alarmTime}
+                                id={props.alarmid + '-time'}
                                 onChange={(event) => setAlarmTime(event.target.value)}
                                 inputProps={{
                                     step: 300
@@ -45,7 +45,7 @@ export default function AlarmForm(props) {
                     </DialogContentText>
                 </DialogContent>
                 <DialogActions>
-                    <Button variant="contained" id='save' color="primary" onClick={() => props.onSave(label, alarmTime)}>
+                    <Button variant="contained" id={props.alarmid + '-save'} color="primary" onClick={() => props.onEdit(label, alarmTime)}>
                         Save
                     </Button>
                 </DialogActions>
